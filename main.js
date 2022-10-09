4c8bf373686d9c063ffa433c9dcd01f2203f066f
michaelchau0424 <michael.chau0424@gmail.com>
2022-10-09 23:21:52 +1100

    completed second vis

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var vg_map = "map.json";
+var vg_bump = "bump.json";
 vegaEmbed("#map_chart", vg_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#bump_chart", vg_bump).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
