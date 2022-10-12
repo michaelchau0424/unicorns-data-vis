89bb1c6d4d5600c75209cef7c080f3b2b631782e
michaelchau0424 <michael.chau0424@gmail.com>
2022-10-13 10:57:47 +1100

    fixed clean data

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var vg_map = "map.json";
 var vg_bump = "bump.json";
+var vg_bar = "bar.json";
 vegaEmbed("#map_chart", vg_map).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 vegaEmbed("#bump_chart", vg_bump).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#bar_chart", vg_bar).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
