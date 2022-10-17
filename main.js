79ddb5345732e7b42465bd99579af9c15d9e48da
michaelchau0424 <michael.chau0424@gmail.com>
2022-10-17 20:08:13 +1100

    final

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var vg_map = "map.json";
 var vg_bump = "bump.json";
 var vg_bar = "bar.json";
-vegaEmbed("#map_chart", vg_map).then(function(result) {
+vegaEmbed("#map_chart", vg_map, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#bump_chart", vg_bump).then(function(result) {
+vegaEmbed("#bump_chart", vg_bump,{"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#bar_chart", vg_bar).then(function(result) {
+vegaEmbed("#bar_chart", vg_bar,{"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
